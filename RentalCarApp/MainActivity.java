5a0c4a266133e88ee691ffb4ffffc3605f4ddf55
Anh Bui <49821510+minhanhbui97@users.noreply.github.com>
2019-05-14 15:01:44 -0500

    update directory

--- RentalCarApp/MainActivity.java
+++ RentalCarApp/MainActivity.java
@@ -1,10 +1,4 @@
-package txstate.edu.amb309.rentalcarapp;
-
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+package [your package name here];
 
 public class MainActivity extends AppCompatActivity {
 
